8b694bad780205e41ff41869ef5f08436c30bcd5
emuikernel <emuidown@sina.com>
2015-12-03 14:11:32 +0800

    Add CP8150 058 files.

---
+++ system/bin/wbf_test.sh
@@ -0,0 +1,12 @@
+echo  "use labtool and mfg "
+	cd /system/lib/modules/
+	insmod mlan.ko
+	insmod  sd8787.ko "mfg_mode=1 drv_mode=1 fw_name=mrvl/sd8787.bin"
+	insmod bt8787.ko
+	cd  /system/bin
+	chmod 777   mfgbridge
+	chmod 777   bridge_init.conf
+	echo 1 >/sys/class/rfkill/rfkill0/state
+	hciconfig hci0 up
+	ifconfig usb0 192.168.1.101 up
+	./labtool 
